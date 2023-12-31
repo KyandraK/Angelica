4646508ac595f41062bbcff2b6f7fde0a74994ef
nanzcakep <nandanugraha107@gmail.com>
2023-12-31 18:59:23 +0700

    FINAL PUSH

--- resources/views/admin/addproduk.blade.php
+++ resources/views/admin/addproduk.blade.php
@@ -34,7 +34,7 @@
                         </g>
                     </svg>
                     Back
-                </a>
+                </a>    
             </div>            
             @if ($errors->any())
             <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
